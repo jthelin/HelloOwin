eb9991f11907587ffa6506f8772732f935bcb977
Jorgen Thelin <jthelin@microsoft.com>
2019-07-06 15:31:38 -0700

    Run tests on both Windows and Linux (#26)
    - Separate NuGet Restore step
    - NuGet restore for Linux build.
    - Install latest nuget version for Linux.
    
    - tests.sh - add param to specify which build configuration to run tests against.
    
    - Change from deprecated use of `xbuild` to using `msbuild`

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+BUILD_CONFIGURATION=${1?="Debug"}
+
+mono packages/xunit.runner.console.*/tools/net46/xunit.console.exe \
+    HelloOwinTests/bin/${BUILD_CONFIGURATION}/HelloOwinTests.dll \
+    -xml TestResults.xml
