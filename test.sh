5218f0a34fb286d09bb671194c2708a5066cc97f
Jorgen Thelin <jthelin@microsoft.com>
2020-03-03 15:16:47 -0800

    Fix test.sh for net47

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 BUILD_CONFIGURATION=${1:-"Debug"}
 
-mono ~/.nuget/packages/xunit.runner.console/*/tools/net46/xunit.console.exe \
-    HelloOwinTests/bin/${BUILD_CONFIGURATION}/net46/HelloOwinTests.dll \
+mono ~/.nuget/packages/xunit.runner.console/*/tools/net47/xunit.console.exe \
+    HelloOwinTests/bin/${BUILD_CONFIGURATION}/*/HelloOwinTests.dll \
     -xml TestResults.xml
