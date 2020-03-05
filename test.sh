1926e8a83335afe8c0875e485a9d933881a35c14
Jorgen Thelin <jthelin@microsoft.com>
2020-03-05 09:20:33 -0800

    Fix file globbing

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 BUILD_CONFIGURATION=${1:-"Debug"}
 
 mono ~/.nuget/packages/xunit.runner.console/*/tools/net47/xunit.console.exe \
-    "HelloOwinTests/bin/${BUILD_CONFIGURATION}/*/HelloOwinTests.dll" \
+    "HelloOwinTests/bin/${BUILD_CONFIGURATION}"/*/HelloOwinTests.dll \
     -xml TestResults.xml
